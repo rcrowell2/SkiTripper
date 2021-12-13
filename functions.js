b915ada4065525259b442522cb38dc5d631333fe
reesecrowell <crowellreese@gmail.com>
2021-12-13 16:56:51 -0500

    added dropdown menu searching functionality, no db

---
+++ functions.js
@@ -0,0 +1,42 @@
+function updateDisplay() {
+		var responseID = $('.searchType').val()
+        console.log(responseID)
+		if(responseID == "all"){
+			$('#secondLevel').removeClass("hidden");
+			$('#secondLevel').addClass("show");
+            $('#state').removeClass("show");
+			$('#pass').removeClass("show");
+			$('#trip').removeClass("show");
+            $('#state').addClass("hidden");
+			$('#pass').addClass("hidden");
+			$('#trip').addClass("hidden");
+		} else if (responseID == "state"){
+			$('#state').removeClass("hidden");
+			$('#state').addClass("show");
+            $('#all').removeClass("show");
+			$('#pass').removeClass("show");
+			$('#trip').removeClass("show");
+            $('#all').addClass("hidden");
+			$('#pass').addClass("hidden");
+			$('#trip').addClass("hidden");
+		} else if (responseID == "pass"){
+			$('#pass').removeClass("hidden");
+			$('#pass').addClass("show");
+            $('#all').removeClass("show");
+			$('#state').removeClass("show");
+			$('#trip').removeClass("show");
+            $('#all').addClass("hidden");
+			$('#state').addClass("hidden");
+			$('#trip').addClass("hidden");
+		} else if (responseID == "trip"){
+			$('#trip').removeClass("hidden");
+			$('#trip').addClass("show");
+            $('#all').removeClass("show");
+			$('#pass').removeClass("show");
+			$('#state').removeClass("show");
+            $('#all').addClass("hidden");
+			$('#pass').addClass("hidden");
+			$('#state').addClass("hidden");
+		}
+}
+
